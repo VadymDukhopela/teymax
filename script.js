92e8371617f871312f229673cf4731aec43b938e
Vadym <kb201_dvo@student.ztu.edu.ua>
2021-11-28 23:01:39 +0200

    не придумал, но оно выводит в консоль

--- script.js
+++ script.js
@@ -35,33 +35,41 @@ document.body.appendChild(contentDiv);
 for (let i = 0; i < arr.length; i++){
   let contentItem = document.createElement('p');
   contentItem.className = 'content__item';
-  contentItem.id = `${arr[i].id}`
+  contentItem.id = `${arr[i].id}`;
   contentItem.innerText = `${arr[i].content}`;
   document.querySelector('#content').appendChild(contentItem);
 }
 
+document.querySelectorAll('.content__item').forEach(item => {
+  item.addEventListener('click', () =>{
+    const id__elemnt = (item.id);
+    for (let i = 0; i < arr.length; i++){
+      for (let j = 0; j < arr[i].relations.length; j++){
+        if (id__elemnt === arr[i].relations[j]){
+          console.log(arr[i].id);
+        }
+      }
+    }
+  });
+});
 
-
-document.querySelectorAll('.content__item').ForEach((item) => {
+/*document.querySelectorAll('.content__item').ForEach((item) => {
   item.addEventListener('click', () => {
     console.log('Даже кликает');
-  })
-})
-
-
-
+  });
+}); */
 
 
 
 //==========consoleRealization==========//
 /*for (let i = 0; i < arr.length; i++){
-  for (let k = 0; k < arr.length; k++){
-    for(let j = 0; j < arr[k].relations.length; j++){
-      if (arr[i].id === arr[k].relations[j]){
-        console.log(`${arr[i].content} \t ${arr[k].content}`);
+    for (let k = 0; k < arr.length; k++){
+      for(let j = 0; j < arr[k].relations.length; j++){
+        if (arr[i].id === arr[k].relations[j]){
+          console.log(`${arr[i].content} \t ${arr[k].content}`);
+        }
       }
     }
   }
-}
 */
 
